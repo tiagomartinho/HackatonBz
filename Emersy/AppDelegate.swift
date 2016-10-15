a7f31a80314500d666f9613c412adc9c2993e2ec
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 15:25:42 +0200

    adds custom actions to notifications

--- Emersy/AppDelegate.swift
+++ Emersy/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
@@ -15,9 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func requestNotificationsAuthorization() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             print("requestNotificationsAuthorizationGranted? \(granted)")
         }
+        center.setNotificationCategories(notificationCategories())
+        center.delegate = self
+    }
+
+    func notificationCategories() -> Set<UNNotificationCategory> {
+        let acceptAction = UNNotificationAction(identifier: "ACCEPT", title: "ACCEPT", options: [.foreground])
+        let refuseAction = UNNotificationAction(identifier: "REFUSE", title: "REFUSE", options: [.destructive, .foreground])
+        let missionCategory = UNNotificationCategory(identifier: "MISSION", actions: [acceptAction, refuseAction], intentIdentifiers: [], options: [])
+        var categories = Set<UNNotificationCategory>()
+        categories.insert(missionCategory)
+        return categories
     }
 
     func application(_ application: UIApplication,
@@ -31,5 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         print("didReceiveRemoteNotification")
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         didReceive response: UNNotificationResponse,
+                                         withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("userNotificationCenter")
+        completionHandler()
     }
 }
