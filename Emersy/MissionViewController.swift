1c25582e2f59b7add5e9c392577db337d8a818c8
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 16:51:29 +0200

    adds mission data to MissionViewController

--- Emersy/MissionViewController.swift
+++ Emersy/MissionViewController.swift
@@ -1,4 +1,16 @@
 import UIKit
 
 class MissionViewController: UITableViewController {
+
+    let missions = ["Mission 1", "Mission 2", "Mission 3"]
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = missions[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return missions.count
+    }
 }
