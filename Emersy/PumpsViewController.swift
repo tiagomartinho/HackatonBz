c5622eb7daacad2500907bb2d8bb243d915c3f85
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 19:37:40 +0200

    long press add annotation to map

--- Emersy/PumpsViewController.swift
+++ Emersy/PumpsViewController.swift
@@ -1,5 +1,19 @@
 import UIKit
+import MapKit
 
 class PumpsViewController: UIViewController {
-    
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    var annotations = [MKPointAnnotation]()
+
+    @IBAction func longPressGesture(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            let touchPoint = sender.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+        }
+    }
 }
