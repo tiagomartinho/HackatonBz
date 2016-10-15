a10d00a551a33093e450ca6e14181f2306395a0b
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 22:06:36 +0200

    changes json structure

--- Emersy/PumpsService.swift
+++ Emersy/PumpsService.swift
@@ -2,6 +2,12 @@ import Alamofire
 
 class PumpsService {
     func send(input: PumpsServiceInput) {
-
+        let url = "http://10.171.21.177:8080/pumpLocation"
+        Alamofire.request(url,
+                          method: .post,
+                          parameters: input.serialized(),
+                          encoding: JSONEncoding.default).response { (response) in
+                            print(response.response?.statusCode)
+        }
     }
 }
--- Emersy/PumpsServiceInput.swift
+++ Emersy/PumpsServiceInput.swift
@@ -9,22 +9,34 @@ struct PumpsServiceInput {
 
         var points = [Any]()
 
+        let first = annotationToPoint(annotations.first!)
+        let last = annotationToPoint(annotations.last!)
+
         for annotation in annotations {
-            let lat = annotation.coordinate.latitude
-            let lng = annotation.coordinate.latitude
-            let point: [String: Any] = [
-                "lat": lat,
-                "lng": lng
-            ]
-            points.append(point)
+            points.append(annotationToPoint(annotation))
         }
+
+        points.removeFirst()
+        points.removeLast()
 
         let parameters: [String: Any] = [
             "pa": pa,
             "flowRate": flowRate,
-            "points" : points
+            "points" : points,
+            "hydrant" : first,
+            "fire" : last
         ]
 
         return parameters
+    }
+
+    func annotationToPoint(_ annotation: MKAnnotation) -> [String: Any] {
+        let lat = annotation.coordinate.latitude
+        let lng = annotation.coordinate.latitude
+        let point: [String: Any] = [
+            "lat": lat,
+            "lng": lng
+        ]
+        return point
     }
 }
