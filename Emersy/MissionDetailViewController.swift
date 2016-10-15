03e97d6ecd1bbc2f507f4a631c4c5e02fe1a67ce
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 17:06:31 +0200

    instantiates directly MissionDetailViewController

--- Emersy/MissionDetailViewController.swift
+++ Emersy/MissionDetailViewController.swift
@@ -3,15 +3,6 @@ import UIKit
 class MissionDetailViewController: UITableViewController {
 
     let users = ["Tiago Martinho", "Juste", "John Doe"]
-
-    var missionName: String?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let missionName = missionName {
-            self.title = missionName
-        }
-    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
--- Emersy/MissionsViewController.swift
+++ Emersy/MissionsViewController.swift
@@ -4,8 +4,6 @@ class MissionsViewController: UITableViewController {
 
     let missions = ["Mission 1", "Mission 2", "Mission 3"]
 
-    var selectedRow: Int?
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = missions[indexPath.row]
@@ -17,11 +15,9 @@ class MissionsViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedRow = indexPath.row
-        performSegue(withIdentifier: "showMissionSegue", sender: self)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.title = missions[selectedRow ?? 0]
+        if let detail = storyboard?.instantiateViewController(withIdentifier: "MissionDetailViewController") {
+            detail.title = missions[indexPath.row]
+            navigationController?.pushViewController(detail, animated: true)
+        }
     }
 }
