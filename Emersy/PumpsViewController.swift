0548b77877c108a00df2f02410e9bbbea55672d5
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-15 20:43:32 +0200

    set first and second annotation colors

--- Emersy/PumpsViewController.swift
+++ Emersy/PumpsViewController.swift
@@ -5,7 +5,7 @@ class PumpsViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
-    var annotations = [MKPointAnnotation]()
+    var annotations = [MKAnnotation]()
 
     let locationManager = CLLocationManager()
     var location: CLLocation?
@@ -37,11 +37,18 @@ class PumpsViewController: UIViewController, MKMapViewDelegate {
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
-            return nil;
+            return nil
         }
 
         let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "reuseIdentifier")
-        annotationView.pinTintColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        switch annotations.count {
+        case 1:
+            annotationView.pinTintColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        case 2:
+            annotationView.pinTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        default:
+            annotationView.pinTintColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        }
         return annotationView
     }
 }
